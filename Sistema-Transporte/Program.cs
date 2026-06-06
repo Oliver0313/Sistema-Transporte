f69e722ca49191b7a427e4edffa75a9ef367de83
Oliver0313 <wiwi.es.org@gmail.com>
2026-06-06 10:41:10 -0400

    Error del CORS solucionado

--- Sistema-Transporte/Program.cs
+++ Sistema-Transporte/Program.cs
@@ -9,10 +9,19 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PermitirVue", policy =>
+    {
+        policy.WithOrigins("http://localhost:5173")
+              .AllowAnyHeader()
+              .AllowAnyMethod();
+    });
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -58,6 +67,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("PermitirVue");
 
 app.UseAuthentication();
 
